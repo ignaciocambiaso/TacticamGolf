fbeccec1ceec6552c4794146be5db6a74a0f5927
Ignacio Cambiaso <ignaciocambiaso@gmail.com>
2020-04-21 12:00:45 -0400

    Cart function modification

--- js/carro.js
+++ js/carro.js
@@ -4,34 +4,38 @@ let products = [
     {
         name: 'Club de Campo Tumbes',
         tag: 'Tumbes',
-        price: 11.990,
+        price: 11990,
         inCart: 0
     },
     {
         name: 'Club de Golf El Alba',
         tag: 'Alba',
-        price: 11.990,
+        price: 11990,
         inCart: 0
     },
     {
         name: 'Club de Campo Bellavista',
         tag: 'Bellavista',
-        price: 11.990,
+        price: 11990,
         inCart: 0
     },
     {
         name: 'Club de Golf La Posada',
         tag: 'Posada',
-        price: 11.990,
+        price: 11990,
         inCart: 0
     }
 ];
 
+
+
 for (let i=0; i < carts.length; i++) {
     carts [i].addEventListener( 'click', () =>{
-        cartNumbers();
+        cartNumbers(products[i]);
+        totalCost(products[i])
     })
 }
+
 
 function onLoadCartNumber (){
     let productNumbers = localStorage.getItem('cartNumbers');
@@ -39,7 +43,8 @@ function onLoadCartNumber (){
         document.querySelector('#top-cart span').textContent = productNumbers;
     }
 }
-function cartNumbers() {
+function cartNumbers(product) {
+   
     let productNumbers = localStorage.getItem('cartNumbers');
 
     productNumbers = parseInt(productNumbers);
@@ -52,6 +57,51 @@ function cartNumbers() {
         localStorage.setItem('cartNumbers', 1);
         document.querySelector('#top-cart span').textContent = 1;
     }
+
+    setItem(product);
+
 }
 
-onLoadCartNumber();
+function setItem(product) {
+   let cartItems = localStorage.getItem('productsInCart');
+   cartItems = JSON.parse(cartItems);
+   
+  if(cartItems !=null) {
+
+    if(cartItems[product.tag] == undefined ) {
+      cartItems = {
+          ...cartItems,
+          [product.tag]: product
+      }      
+    }
+      cartItems[product.tag].inCart += 1;
+  } else {
+   product.inCart = 1;
+   cartItems = {
+        [product.tag]: product
+      } 
+    }
+
+  localStorage.setItem("productsInCart", JSON.stringify(cartItems));
+
+}
+
+function totalCost(product) {
+    //console.log("the product price is", product.price);
+    let cartCost = localStorage.getItem ('totalCost');
+   
+    console.log("My cartCost is", cartCost);
+    console.log(typeof cartCost );
+
+    if(cartCost != null) {
+        cartCost = parseInt(cartCost);
+        localStorage.setItem("totalCost", cartCost + product.price);
+
+    } else {
+        localStorage.setItem("totalCost", product.price);
+
+    }
+
+}
+
+onLoadCartNumber(); 
